686b23b86e0414fea779ed6d19a22a65727718a7
dignityc <dignityc@gmail.com>
2024-09-23 12:37:06 +0100

    clean code

--- LLM_70B copy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-
-#SBATCH --output=/scratch/prj/inf_wqp/ISWC_paper/70B_log.out
-#SBATCH --job-name=gpu
-#SBATCH --gres=gpu:1
-#SBATCH --constraint=a100_80g
-
-nvidia-smi
-python LLMlabeling_70B.py
--- PubMedDBbuilding.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l
-
-#SBATCH --output=/scratch/prj/inf_wqp/ISWC_paper/log.out
-#SBATCH --job-name=gpu
-#SBATCH --gres=gpu:1
-
-python PubMedReading.py
